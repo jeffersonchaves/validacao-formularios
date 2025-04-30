b864a4ee874cff22e292405a1049971a44cf5cd2
jefferson-chaves <jefferson.chaves@ifpr.edu.br>
2025-04-30 16:00:05 -0300

    feat: update some files

--- src/main/java/com/example/validacao_formularios/controllers/EstudanteController.java
+++ src/main/java/com/example/validacao_formularios/controllers/EstudanteController.java
@@ -1,23 +1,51 @@
 package com.example.validacao_formularios.controllers;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.validacao_formularios.models.Estudante;
+
+import jakarta.validation.Valid;
 
 
 @Controller
 public class EstudanteController {
 
     @GetMapping("/estudante")
-    public String cadastro() {
+    public String cadastro(Estudante estudante) {
         return "estudante-cadastro";
     }
 
     @PostMapping("/estudante")
-    public String processarCadastro(Estudante estudante, Model model) {
+    public String processarCadastro(@Valid Estudante estudante, BindingResult fields, Model model) throws IOException {
+
+        //Deveria estar em um lugar compartilhado
+        String UPLOAD_DIR = "src/main/resources/static/uploads/";
+
+        if (fields.hasErrors()) {
+            return "estudante-cadastro";
+        }
+
+        if (estudante.getAvatar() != null && !estudante.getAvatar().isEmpty()) {
+            
+            String fileName = estudante.getAvatar().getOriginalFilename();
+
+            Path path = Paths.get(UPLOAD_DIR + fileName);
+
+            Files.createDirectories(path.getParent());
+            Files.copy(estudante.getAvatar().getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+
+        }
 
 
 
--- src/main/java/com/example/validacao_formularios/models/Estudante.java
+++ src/main/java/com/example/validacao_formularios/models/Estudante.java
@@ -2,17 +2,23 @@ package com.example.validacao_formularios.models;
 
 import java.time.LocalDate;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class Estudante {
 
     @NotNull(message = "o nome não pode estar vazio")
-    @Size(min = 1, max = 100)
+    @Size(min = 1, max = 5, message = "o tamanho nome deve ser entre 1 e 5 caracteres")
     private String nome;
-    
+
     private LocalDate dataNascimento;
+
+    @Size(min = 1, max = 5, message = "o tamanho de matricula deve ser entre 1 e 5 caracteres")
     private String matricula;
+
+    private MultipartFile avatar;
     
     public Estudante(){}
 
@@ -44,6 +50,14 @@ public class Estudante {
 
     public void setMatricula(String matricula) {
         this.matricula = matricula;
+    }
+
+    public MultipartFile getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(MultipartFile avatar) {
+        this.avatar = avatar;
     }
 
     
